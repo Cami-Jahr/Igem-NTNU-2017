ab32549517d08523c1df554c0a109c37fb144120
Joachim <joachimjahr@hotmail.com>
2017-06-12 17:11:48 +0200

    Just throw things at it 'til it works

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 /**
  * Created by Joachim on 12/06/2017.
  */
+
+$(document).ready(function () {
+    $(".menu_item").hover(
+        function() {
+            $(this).find(".menu_column").css("visibility", "visible")
+        },
+        function () {
+            $(this).find(".menu_column").css("visibility", "hidden")
+    })
+});
