8b79677c9994ca94dc51640a39dcd2734232f85c
Joachim <joachimjahr@hotmail.com>
2017-06-15 22:17:43 +0200

    finalised text and borders

--- IGEM/Home/home_script.js
+++ IGEM/Home/home_script.js
@@ -1,6 +1,14 @@
 /**
  * Created by Joachim on 14/06/2017.
  */
+$(window).resize(function() {
+    var div = $('#menu_field');
+    var width = div.width();
+    div.css('height', width * 0.5);
+    $(".break_header").css("top",-width * 0.05);
+
+});
+
 $(function() {
     var div = $('#menu_field');
     var width = div.width();
