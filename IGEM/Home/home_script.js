c9c948e34a2789f41b9c7c8252bef9638a7abc05
Joachim <joachimjahr@hotmail.com>
2017-06-15 16:12:02 +0200

    Added text and seperators

--- IGEM/Home/home_script.js
+++ IGEM/Home/home_script.js
@@ -4,8 +4,7 @@
 $(function() {
     var div = $('#menu_field');
     var width = div.width();
-    console.log(width);
     div.css('height', width * 0.5);
-    $(".break_1").css("top",-width * 0.05);
+    $(".break_header").css("top",-width * 0.05);
 
 });
