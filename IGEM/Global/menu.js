71d0910b02353106d7425d93e7d8c6200e36fedd
Joachim <joachimjahr@hotmail.com>
2017-06-18 16:25:08 +0200

    Menu bar added

--- IGEM/Global/menu.js
+++ IGEM/Global/menu.js
@@ -1,19 +1,3 @@
 /**
  * Created by Joachim on 16/06/2017.
  */
-function slider() {
-    if (document.body.scrollTop > 100) //Show the slider after scrolling down 100px
-        $('#slider').stop().animate({"margin-left": '0', "margin-top": "100"});
-    else
-        $('#slider').stop().animate({"margin-left": '-200', "margin-top": "0"}, 100); //200 matches the width of the slider
-}
-
-
-$(window).scroll(function () {
-    slider();
-});
-
-
-$(document).ready(function () {
-    slider();
-});
--- IGEM/Home/home_script.js
+++ IGEM/Home/home_script.js
@@ -6,7 +6,8 @@ var pricture_field_resize = function () {
     var div = $('#picture_field');
     var width = div.width();
     div.css('height', width * 0.5);
-    $(".break_header").css("top",(-width * 0.05)+20);
+    $(".break_header").css("top",(-width * 0.05)+20 + 37);
+    $("#slider").css("width", width);
 };
 
 //Call on window resize and document startup
