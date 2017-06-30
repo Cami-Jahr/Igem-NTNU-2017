c550ea4ea62932f9ffb9097e8c8dec7b418e10bd
Joachim <joachimjahr@hotmail.com>
2017-06-30 17:14:59 +0200

    Added code to counteract IGEM site's default settings

--- IGEM/Home/picture_field_resize.js
+++ IGEM/Home/picture_field_resize.js
@@ -2,7 +2,7 @@ var pricture_field_resize = function () {
     var div = $('#picture_field');
     var width = div.width();
     div.css('height', width * 0.5);
-    $(".break_header").css("top",(-width * 0.05)+20 + 37);
+    $(".break_header").css("top",(-width * 0.05)+27);
     $("#menu").css("width", width);
 };
 
